8da35ae26bbfdd701f5db6ec7abd424268063c5c
Marilia Vilar <marilia@roadmaps.com.br>
2022-12-07 09:06:16 -0300

    🚚 (#S02V06) Fragments: Organizing the project structure

--- app/src/main/java/com/mariliavilar/todoappcleanarchitecture/fragments/addTask/AddTaskFragment.kt
+++ app/src/main/java/com/mariliavilar/todoappcleanarchitecture/fragments/addTask/AddTaskFragment.kt
@@ -1,20 +1,21 @@
-package com.mariliavilar.todoappcleanarchitecture
+package com.mariliavilar.todoappcleanarchitecture.fragments.addTask
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mariliavilar.todoappcleanarchitecture.R
 
 
-class EditTaskFragment : Fragment() {
+class AddTaskFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_task, container, false)
+        return inflater.inflate(R.layout.fragment_add_task, container, false)
     }
 
 }
--- app/src/main/java/com/mariliavilar/todoappcleanarchitecture/fragments/toDoList/ToDoListFragment.kt
+++ app/src/main/java/com/mariliavilar/todoappcleanarchitecture/fragments/toDoList/ToDoListFragment.kt
@@ -1,10 +1,11 @@
-package com.mariliavilar.todoappcleanarchitecture
+package com.mariliavilar.todoappcleanarchitecture.fragments.toDoList
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mariliavilar.todoappcleanarchitecture.R
 
 
 class ToDoListFragment : Fragment() {
